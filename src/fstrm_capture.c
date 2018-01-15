a7c2100d0c74258e6d6847e4282595aa53a50881
chris mikkelson <chris@mikk.net>
2018-01-15 16:31:12 -0600

    Include netinet/in.h in fstrm_capture and tcp test source.

--- src/fstrm_capture.c
+++ src/fstrm_capture.c
@@ -15,6 +15,7 @@
  */
 
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/un.h>
--- t/test_fstrm_io_sock.c
+++ t/test_fstrm_io_sock.c
@@ -15,6 +15,7 @@
  */
 
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
