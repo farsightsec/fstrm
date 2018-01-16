f849f2948711b5567776d15b2a8d6e504d98cbe1
Chris Mikkelson <cmikk@fsi.io>
2018-01-16 13:03:28 -0600

    fstrm_replay: correct arguments to fstrm_writer_write() call

--- src/fstrm_replay.c
+++ src/fstrm_replay.c
@@ -54,16 +54,6 @@ process_start_frame(struct fstrm_reader *r, struct fstrm_writer_options *wopt)
 	return fstrm_res_success;
 }
 
-static fstrm_res
-write_data_frame(struct fstrm_writer *w, const uint8_t *data, size_t len_data)
-{
-	struct iovec iov = {
-		.iov_base = (void *) data,
-		.iov_len = len_data
-	};
-	return fstrm_writer_write(w, &iov, 1);
-}
-
 int main(int argc, char **argv)
 {
 	const char *output_type = NULL;
@@ -163,7 +153,7 @@ int main(int argc, char **argv)
 		res = fstrm_reader_read(r, &data, &len_data);
 		if (res == fstrm_res_success) {
 			/* Write the data frame. */
-			res = write_data_frame(w, data, len_data);
+			res = fstrm_writer_write(w, data, len_data);
 			if (res != fstrm_res_success) {
 				fprintf(stderr, "Error: write_data_frame() failed.\n");
 				goto out;
