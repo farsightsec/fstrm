3302b2fcc47a82f11c61631d550b584ab420c37f
Chris Mikkelson <cmikk@fsi.io>
2018-01-16 11:37:58 -0600

    Rebuild html documentation from commit c9540462020064d07692a27e8114b033d342faee (tags/v0.3.2) using Doxygen 1.8.13

--- group__fstrm__rdwr.js
+++ group__fstrm__rdwr.js
@@ -1,10 +1,10 @@
 var group__fstrm__rdwr =
 [
-    [ "fstrm_rdwr_destroy_func", "group__fstrm__rdwr.html#ga7e1bab0ee71c581c0e6ba7a597a9ef38", null ],
-    [ "fstrm_rdwr_open_func", "group__fstrm__rdwr.html#ga89a2651540c18e516e666b8bd0f5ef48", null ],
-    [ "fstrm_rdwr_close_func", "group__fstrm__rdwr.html#ga89f2862d3be7ea9bf8770b7b20cc955e", null ],
-    [ "fstrm_rdwr_read_func", "group__fstrm__rdwr.html#ga5590ffdb14c977580b0f2e1c28354a92", null ],
-    [ "fstrm_rdwr_write_func", "group__fstrm__rdwr.html#ga79ee0fe10dc47da1b83daadcdc951120", null ],
+    [ "fstrm_rdwr_destroy_func", "group__fstrm__rdwr.html#gaf04e442356af858f22e65ab804181371", null ],
+    [ "fstrm_rdwr_open_func", "group__fstrm__rdwr.html#gaa641afb182a5368466ee1191097651b8", null ],
+    [ "fstrm_rdwr_close_func", "group__fstrm__rdwr.html#ga75671adc1ddb8c24e54fc1e133fd295a", null ],
+    [ "fstrm_rdwr_read_func", "group__fstrm__rdwr.html#gafc15774fbfed96f66ba35ab7ae7de0e5", null ],
+    [ "fstrm_rdwr_write_func", "group__fstrm__rdwr.html#ga09616ebd3eb56c7c64d0f143ee494e23", null ],
     [ "fstrm_rdwr_init", "group__fstrm__rdwr.html#ga5fc7f423d1b078f301dfd6dd693312c6", null ],
     [ "fstrm_rdwr_destroy", "group__fstrm__rdwr.html#gac4400937c7ea1cccaf366c889029357e", null ],
     [ "fstrm_rdwr_open", "group__fstrm__rdwr.html#ga300a307b39d12b7276eb3a6a2b0a09de", null ],
---
+++ navtreedata.js
@@ -0,0 +1,21 @@
+var NAVTREE =
+[
+  [ "fstrm", "index.html", [
+    [ "Introduction", "index.html", null ],
+    [ "Library overview", "overview.html", [
+      [ "Initializing the library", "overview.html#init", null ],
+      [ "Getting an input queue", "overview.html#queue", null ],
+      [ "Submitting data frames", "overview.html#submit", null ],
+      [ "Shutting down", "overview.html#shutdown", null ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group__fstrm__control.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
